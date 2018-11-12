35f5c988797d88ac8e0b30fa5a03d8f682ea6daf
ArthurFeng <kuanchungfeng@yahoo.com.tw>
2018-11-12 17:35:06 +0800

    修改錯誤

---
+++ src/main/java/com/torr/service/impl/RoleServiceImpl.java
@@ -0,0 +1,45 @@
+package com.torr.service.impl;
+
+import com.torr.domain.Role;
+import com.torr.domain.Student;
+import com.torr.repository.RoleDao;
+import com.torr.service.RoleService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
+import java.util.List;
+
+
+@Service
+@Transactional
+public class RoleServiceImpl implements RoleService {
+
+    @Resource
+    private RoleDao roleDao;
+
+    @Override
+    public Role insert(Role role) {
+        return roleDao.insert(role);
+    }
+
+    @Override
+    public void delete(Role role) {
+        roleDao.delete(role);
+    }
+
+    @Override
+    public Role update(Role role) {
+        return roleDao.update(role);
+    }
+
+    @Override
+        public List<Role> findAll() {
+            return roleDao.findAll();
+        }
+
+    @Override
+    public Role findOne(String Id) {
+        return roleDao.findOne(Id);
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -46,12 +46,12 @@ CREATE TABLE ROLES(
     CAREER_ VARCHAR (20),
 );
 
-INSERT INTO Roles(name_,atk_,hp_,career_) VALUES ('The Majestic Premier',99,589,'swordman');     //曹操
-INSERT INTO Roles(name_,atk_,hp_,career_)  VALUES ('Brain of The Darkness',258,469,'adviser');     //司馬懿
-INSERT INTO Roles(name_,atk_,hp_,career_) VALUES ('The Lion-Hearted King',88,555,'swordman');    //孫堅
-INSERT INTO Roles(name_,atk_,hp_,career_) VALUES ('The Angel of Wrath',150,450,'swordman');      //孫尚香
-INSERT INTO Roles(name_,atk_,hp_,career_)  VALUES ('The Lord of Virtue',80,699,'adviser');         //劉備
-INSERT INTO Roles(name_,atk_,hp_,career_) VALUES ('The God of Battle',180,650,'swordman');        //關羽
-INSERT INTO Roles(name_,atk_,hp_,career_) VALUES ('The Strength',199,632,'swordman');             //張飛
-INSERT INTO Roles(name_,atk_,hp_,career_)  VALUES ('The Wizard of Fortune',500,500,'adviser');      //諸葛亮
-INSERT INTO Roles(name_,atk_,hp_,career_) VALUES ('Violence Hurricane',230,1000,'swordman');       //呂布
+INSERT INTO Roles(name_,atk_,hp_,career_) VALUES ('The Majestic Premier',99,589,'swordman');    -- //曹操
+INSERT INTO Roles(name_,atk_,hp_,career_)  VALUES ('Brain of The Darkness',258,469,'adviser');  --   //司馬懿
+INSERT INTO Roles(name_,atk_,hp_,career_) VALUES ('The Lion-Hearted King',88,555,'swordman');   -- //孫堅
+INSERT INTO Roles(name_,atk_,hp_,career_) VALUES ('The Angel of Wrath',150,450,'swordman');     -- //孫尚香
+INSERT INTO Roles(name_,atk_,hp_,career_)  VALUES ('The Lord of Virtue',80,699,'adviser');      --   //劉備
+INSERT INTO Roles(name_,atk_,hp_,career_) VALUES ('The God of Battle',180,650,'swordman');      --  //關羽
+INSERT INTO Roles(name_,atk_,hp_,career_) VALUES ('The Strength',199,632,'swordman');           --  //張飛
+INSERT INTO Roles(name_,atk_,hp_,career_)  VALUES ('The Wizard of Fortune',500,500,'adviser');  --    //諸葛亮
+INSERT INTO Roles(name_,atk_,hp_,career_) VALUES ('Violence Hurricane',230,1000,'swordman');    --   //呂布
