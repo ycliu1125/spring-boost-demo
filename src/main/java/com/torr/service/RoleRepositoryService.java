2b5cfbab404d0bdaf118b40862e035c7d3d21f76
ArthurFeng <kuanchungfeng@yahoo.com.tw>
2018-11-15 14:59:36 +0800

    修改RoleRepository(還沒做單元測試)

--- src/main/java/com/torr/service/RoleRepositoryService.java
+++ src/main/java/com/torr/service/RoleRepositoryService.java
@@ -1,20 +1,20 @@
-package com.torr.service;
-
-import com.torr.domain.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface RoleRepositoryService extends JpaRepository<Role,String> {
-    Role insert(Role role);
-
-    void delete(Role role);
-
-    Role update(Role role);
-
-    List<Role> findAll();
-
-    Role findOne(String Id);
-
-    Role findByCareer(String career);
-}
+//package com.torr.service;
+//
+//import com.torr.domain.Role;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//
+//public interface RoleRepositoryService extends JpaRepository<Role,String> {
+//    Role insert(Role role);
+//
+//    void delete(Role role);
+//
+//    Role update(Role role);
+//
+//    List<Role> findAll();
+//
+//    Role findOne(String Id);
+//
+//    Role findByCareer(String career);
+//}
