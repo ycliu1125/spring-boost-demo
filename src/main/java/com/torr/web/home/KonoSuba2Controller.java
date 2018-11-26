15287c2536413da72767b5e0820bc3a84b881a15
ycliu1125 <ycliu1125@gmail.com>
2018-11-26 14:19:05 +0800

    Maintain介面完成，缺按鈕功能實作及input介面

--- src/main/java/com/torr/web/home/KonoSuba2Controller.java
+++ src/main/java/com/torr/web/home/KonoSuba2Controller.java
@@ -1,22 +1,43 @@
 package com.torr.web.home;
 
 
+import com.torr.domain.KonoSuba;
+import com.torr.service.KonoSubaService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("")
 public class KonoSuba2Controller {
+
+    @Resource
+    private KonoSubaService konoSubaService;
 
     @RequestMapping("/index")
     public String konosuba2(){
         return "index.html";
     }
 
-    //維護介面
-    @RequestMapping("/maintain")
-    public String maintain(){
-        return "maintain.html";
+    //POST 處理新增
+    @PostMapping("/maintain")
+    public String save(
+            @Valid @ModelAttribute("main") KonoSuba main,
+            BindingResult bindingResult,
+            Model model){
+        String willReturn = "pages/maintain.html";
+        if (bindingResult.hasErrors()) {
+            willReturn = "pages/maintain.html";
+        } else {
+            konoSubaService.insert(main);
+        }
+        return willReturn;
     }
 
 
